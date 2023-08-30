3c2ebb124a9caa8a700ee6702d6fd1622be92cd2
Ibuu31 <ibrahimbode41@gmail.com>
2023-08-30 20:58:05 +0530

    The UI part of the app is ready.

---
+++ lib/client_api.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:weather_app/weather_model.dart';
+
+class WeatherClientApi {
+  Future<WeatherModel> requestApi() async {
+    String url =
+        'https://api.openweathermap.org/data/2.5/weather?lat=23.0225&lon=72.5714&appid=198c5d70f721bee7ab433ba9e748035b';
+    Response response = await Dio().get(url);
+    Map<String, dynamic> parsedData = jsonDecode(response.toString());
+    final weather = WeatherModel.fromJson(parsedData);
+    debugPrint(parsedData.toString());
+    return weather;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/weather_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MyHomePage();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({
+    super.key,
+  });
+  
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: WeatherScreen(),
+      ),
+    );
+  }
+}
